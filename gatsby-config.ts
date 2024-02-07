47f7e53ab949f1e3d2a511c65ea765daeda50eac
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-07 15:40:42 +0000

    update title and version

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `justNew`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Gatsby playground`,
+    siteUrl: `https://gatsby-playgournd.localhost`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
